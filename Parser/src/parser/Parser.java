85f38ae916686e3503b4c939dd9b9bedfda4a005
Michael Johnson <mrjohnson2@buffs.wtamu.edu>
2017-02-02 13:45:48 -0600

    Finished preliminary implementation of Parser class. Still need to add more syntax error checking and verify correctness.

--- Parser/src/parser/Parser.java
+++ Parser/src/parser/Parser.java
@@ -1,10 +1,10 @@
 
 package parser;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 
 /**
  *
@@ -20,7 +20,8 @@ import java.io.FileNotFoundException;
  */
 public class Parser {
 
-    protected ArrayList<State> stateList;
+    protected static HashMap<String, State> stateList;
+    private static boolean PARSER_ERROR = false;
     private static final int[] TOKEN_SEQUENCE = {
         Token.STATE, Token.SYMBOL, Token.SYMBOL, Token.DIRECTION, Token.STATE
     };
@@ -28,22 +29,43 @@ public class Parser {
     public static void parseSourceFile(String filePath) throws FileNotFoundException {
         Scanner sourceFile = new Scanner(new File(filePath));
 
-		while (sourceFile.hasNextLine()) {
-            for (int token : TOKEN_SEQUENCE) {
-                String lexeme = sourceFile.next();
-                if (Token.lexemeMatches(Token.COMMENT, lexeme)) {
-                    sourceFile.nextLine();
-                    break;
-                }
-                if (Token.lexemeMatches(token, lexeme)) {
-                    System.out.println("Found token " + token + ": " + lexeme);
-                } else {
-                    System.out.println("Expected token " + token + " but got: " + lexeme);
-                }
-            }
-
-		}
-		sourceFile.close();
+	while (sourceFile.hasNextLine()) {
+            parseSourceLine(sourceFile.nextLine());
+	}
+	sourceFile.close();
     }
     
+    private static void parseSourceLine(String sourceLine) {
+        if (isCommentLine(sourceLine)) {
+            return;
+        }
+        String[] lexemes = sourceLine.split("\\s+");
+        for (int i = 0; i < TOKEN_SEQUENCE.length; i++) {
+            if (!Token.lexemeMatches(TOKEN_SEQUENCE[i], lexemes[i])) {
+                printParserError(TOKEN_SEQUENCE[i], lexemes[i]);
+                PARSER_ERROR = true;
+                break;
+            }
+        }
+        if (!PARSER_ERROR) {
+            addStateTransition(lexemes);
+        }
+    }
+    
+    private static boolean isCommentLine(String sourceLine) {
+        return Token.lexemeMatches(Token.COMMENT, sourceLine);
+    }
+    
+    private static void addStateTransition(String[] lexemes) {
+        State currentState = stateList.get(lexemes[0]);
+        if (currentState == null) {
+            currentState = new State(lexemes[0]);
+        }
+        currentState.addTransition(new Transition(lexemes[1], lexemes[2],
+                lexemes[3], lexemes[4]));
+    }
+    
+    private static void printParserError(int token, String lexeme) {
+        System.out.println("Expected token " + token + " but got: " + lexeme);
+    }
 }
