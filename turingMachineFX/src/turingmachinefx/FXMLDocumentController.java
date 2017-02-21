e4e61af7222c6211f2ab00bb28f87968a6fc15ed
Anthony Thornton <althornton1@buffs.wtamu.edu>
2017-02-21 14:19:08 -0600

    Added code to buttons in FX

--- turingMachineFX/src/turingmachinefx/FXMLDocumentController.java
+++ turingMachineFX/src/turingmachinefx/FXMLDocumentController.java
@@ -23,6 +23,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import Controller.TMController;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -34,6 +38,33 @@ public class FXMLDocumentController implements Initializable {
     private Button stepButton;
     @FXML
     private TextField currentState;
+    @FXML
+    private TextField currentSymbol;
+    @FXML
+    private TextField newSymbol;
+    @FXML
+    private TextField direction;
+    @FXML
+    private TextField newState;
+    @FXML
+    private TextField initialState;
+    @FXML
+    private TextField input;
+    @FXML
+    private TextField tape;
+    @FXML
+    private Button startButton1;
+    @FXML
+    private Button stepButton1;
+    @FXML
+    private Button stopButton1;
+    @FXML
+    private Button resetButton1;
+    @FXML
+    private Button loadButton1;
+    @FXML
+    private Label instructionCount;
+    private TMController controller;
     
     FileChooser fileChooser = new FileChooser();
     
@@ -48,6 +79,39 @@ public class FXMLDocumentController implements Initializable {
         Node node = (Node) event.getSource();
         File file = fileChooser.showOpenDialog(node.getScene().getWindow());
         startSourceView(file);
+        
+        /*try {
+            controller.loadData(input.getText(),initialState.getText());
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tape.setText(input.getText());
+        startButton1.setDisable(false);
+        startButton1.setDisable(false);
+        stepButton1.setDisable(false);
+        stopButton1.setDisable(false);
+        input.setEditable(false);
+        loadButton1.setDisable(true);
+        setNextState();
+        */
+    }
+    @FXML
+    private void handleResetButtonAction(ActionEvent event) {
+        controller.resetData();
+        input.setEditable(true);
+        tape.setText(input.getText());
+        startButton1.setDisable(true);
+        stepButton1.setDisable(true);
+        stopButton1.setDisable(true);
+        instructionCount.setText("0");
+        loadButton1.setDisable(false);
+}
+    @FXML
+    private void handleStepButtonAction(ActionEvent event){
+        controller.step();
+        setTape();
+        setNextState();
+        instructionCount.setText(String.valueOf(controller.getIC()));
     }
     
     private void startSourceView(File sourceFile) {
@@ -72,6 +136,33 @@ public class FXMLDocumentController implements Initializable {
         }
         sourceView.setText(sb.toString());
     }
+    
+    /**
+ * Gets data from controller and populates the boxes on the GUI that explain the
+ * next step on the turing machine
+ */
+    public void setNextState(){
+        String[] values = controller.getData();
+        currentState.setText(values[0]);
+        currentSymbol.setText(values[1]);
+        newSymbol.setText(values[2]);
+        direction.setText(values[3]);
+        newState.setText(values[4]);
+        
+    }
+/**
+ * Gets the value of the tape stored in the controller method and stores it in the
+ * tape field in the GUI.
+ */
+    public void setTape(){
+        Vector<Character> tapeVector = controller.getTape();
+        String newTape = new String();
+        for(int i = 0; i < tapeVector.size(); i++){
+            newTape = newTape + tapeVector.get(i);
+        }
+        tape.setText(newTape);
+    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
